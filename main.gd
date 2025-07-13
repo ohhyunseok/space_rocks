6c686f452d15a04096fa3c7bae65b707267669be
wisestone80 <ohshaker@gmail.com>
2025-07-13 23:40:37 +0900

    Add HUD and integrate lives and score system
    Introduces a new HUD scene and script to display score, lives, and messages. Updates main and player scripts to manage game state, scoring, and player lives, and connects relevant signals between player, HUD, and main. Also updates scene files to instantiate and wire up the new HUD and player features.

--- main.gd
+++ main.gd
@@ -3,11 +3,14 @@ extends Node
 @export var rock_scene : PackedScene
 
 var screensize = Vector2.ZERO
+var level = 0
+var score = 0
+var playing = false
 
 func _ready() -> void:
 	screensize = get_viewport().get_visible_rect().size
-	for i in 3:
-		spawn_rock(3)
+	#for i in 3:
+		#spawn_rock(3)
 		
 func spawn_rock(size, pos=null, vel=null):
 	if pos == null:
@@ -29,4 +32,31 @@ func _on_rock_exploded(size, radius, pos, vel):
 		var newpos = pos + dir * radius
 		var newvel = dir * vel.length() * 1.1
 		spawn_rock(size - 1, newpos, newvel)
+	score += 10 * size
 		
+func new_game():
+	get_tree().call_group("rocks", "queue_free")		
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
+		spawn_rock(3)
+
+func  _process(delta):
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
+		
+func game_over():
+	playing = false
+	$HUD.game_over()
+	
--- player.gd
+++ player.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
+signal lives_changed
+signal dead
+
+var reset_pos = false
+var lives = 0: set = set_lives
+
 @export var engin_power = 500
 @export var spin_power =800
 @export var bullet_scene : PackedScene
@@ -54,6 +60,9 @@ func _integrate_forces(physics_state: PhysicsDirectBodyState2D) -> void:
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
 	
 func shoot():
 	if state == INVULNERABLE:
@@ -64,6 +73,19 @@ func shoot():
 	get_tree().root.add_child(b)
 	b.start($Muzzle.global_transform)
 
-
 func _on_gun_cool_down_timeout() -> void:
 	can_shoot = true
+
+func set_lives(value):
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
